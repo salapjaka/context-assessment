ee14b67060c7f6deb2908a19f50ec2f73c1070c8
sandrabosk <sandraboskovic@hotmail.com>
2019-05-18 18:04:31 -0400

    add code with drilling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { MyContext } from './context';
+
+function A () {
+    return (
+      <MyContext.Consumer>
+        {context => {
+          return(
+            <div className="component-A">
+              <h1>Component A</h1>
+                context/state in Component A => {context.x} <br />
+                <B />
+                <D />
+            </div>
+          )
+        }}
+      </MyContext.Consumer>
+    );
+}
+function B () {
+  return(
+    <MyContext.Consumer>
+      {context => {
+        return (
+          <div className="component-B">
+            <h1>Component B</h1>
+            context/state in Component B => {context.x} <br />
+            <C />
+          </div>
+        );
+      }}
+    </MyContext.Consumer>
+  )
+    
+  
+}
+function C (){
+    return (
+      <MyContext.Consumer>
+        { context => {
+          return(
+            <div className="component-C">
+              <h1>Component C</h1>
+              context/state in Component C => {context.x} <br />
+                <button onClick={context.onXClick}>{context.x}</button> onClick =>
+                context.onXClick sets the state in context
+            </div>
+          )
+        } }
+      </MyContext.Consumer>
+    );
+
+}
+function D () {
+
+    return (
+      <MyContext.Consumer>
+      { context => {
+        return(
+          <div className="component-D">
+            <h1>Component D</h1>
+            context/state in Component D => {context.x} <br />
+            <button onClick={context.onXClick}>{context.x}</button> onClick =>
+            context.onXClick sets the state in context
+          </div>
+        )
+      } } 
+      </MyContext.Consumer>
+    );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <A />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
